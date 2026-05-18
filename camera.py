4727c6c2b1536e86005420db243023747fbb620b
hiroto09 <k23015kk@aitech.ac.jp>
2026-05-18 17:22:37 +0900

    up

--- camera.py
+++ camera.py
@@ -1,10 +1,24 @@
 from picamera2 import Picamera2
+from picamera2.preview import Preview
 from time import sleep
 
 picam2 = Picamera2()
+
+# プレビュー開始
+picam2.start_preview(Preview.QTGL)
+
+# カメラ開始
 picam2.start()
 
-sleep(2)
+print("camera preview start")
 
-picam2.capture_file("test.jpg")
-print("saved")
+try:
+    while True:
+        sleep(1)
+
+except KeyboardInterrupt:
+    print("stop")
+
+finally:
+    picam2.stop_preview()
+    picam2.stop()
