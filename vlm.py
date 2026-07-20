9c12419791af5616942a5ac493c0a508a0d89ac9
hiroto09 <k23015kk@aitech.ac.jp>
2026-07-20 17:44:39 +0900

    update:camera

--- vlm.py
+++ vlm.py
@@ -228,11 +228,13 @@ try:
 
         current_frame = picam2.capture_array()
 
-        preview = cv2.cvtColor(
-        current_frame,
-        cv2.COLOR_BGRA2BGR
+        print(current_frame.shape)
+
+        cv2.imshow(
+            "Preview",
+            current_frame
         )
-        cv2.imshow("Preview", preview)
+
         cv2.waitKey(1)
 
         if has_changed(previous_frame, current_frame):
